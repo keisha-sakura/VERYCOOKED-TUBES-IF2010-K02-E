9ab006543144971949e609ad5bf22417c15d9855
Sen-cpu1 <18224100@std.stei.itb.ac.id>
2025-12-12 10:23:23 +0000

    feat : add TrashStation.java

---
+++ src/main/java/model/station/TrashStation.java
@@ -0,0 +1,46 @@
+
+package model.station;
+
+import model.core.Position;
+import model.entity.Chef;
+import model.enums.StationType;
+import model.item.Item;
+import model.item.utensils.KitchenUtensil;
+
+
+public class TrashStation extends Station {
+    
+    public TrashStation(Position position) {
+        super(position, StationType.TRASH);
+    }
+    
+    @Override
+    public void interact(Chef chef) {
+        Item heldItem = chef.getInventory();
+        
+        if (heldItem == null) {
+            return;
+        }
+        
+        // buang isi kitchen utensil saja
+        if (heldItem instanceof KitchenUtensil) {
+            KitchenUtensil utensil = (KitchenUtensil) heldItem;
+            utensil.clear();
+            System.out.println("Cleared contents of " + utensil.getName());
+        } else {
+            // Buang item sepenuhnya
+            chef.setInventory(null);
+            System.out.println("Trashed item");
+        }
+    }
+    
+    @Override
+    public boolean canInteract(Chef chef) {
+        return chef.getInventory() != null;
+    }
+    
+    @Override
+    public String getInteractionPrompt() {
+        return "Press V to trash item";
+    }
+}
