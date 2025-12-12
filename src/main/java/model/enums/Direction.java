764ec68070adfdd16a8a00549486271a0f899bdd
meleppz <melvin.irsyad2201@gmail.com>
2025-12-12 16:50:25 +0700

    fix: fix direction

--- src/main/java/model/enums/Direction.java
+++ src/main/java/model/enums/Direction.java
@@ -1,21 +1,19 @@
 package main.java.model.enums;
 
 public enum Direction {
-    UP, DOWN, LEFT, RIGHT;
+    UP(0, -1),
+    DOWN(0, 1),
+    LEFT(-1, 0),
+    RIGHT(1, 0);
 
-    public int getDx() {
-        return switch(this) {
-            case RIGHT -> 1;
-            case LEFT -> -1;
-            default -> 0;
-        };
+    private final int dx;
+    private final int dy;
+
+    Direction(int dx, int dy) {
+        this.dx = dx;
+        this.dy = dy;
     }
 
-    public int getDy() {
-        return switch(this) {
-            case DOWN -> 1;
-            case UP -> -1;
-            default -> 0;
-        };
-    }
+    public int getDx() { return dx; }
+    public int getDy() { return dy; }
 }
