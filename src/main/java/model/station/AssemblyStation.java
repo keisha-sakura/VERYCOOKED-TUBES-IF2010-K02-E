bc145374875836234882d193f77094bd0ce22a57
Sen-cpu1 <18224100@std.stei.itb.ac.id>
2025-12-12 21:14:14 +0000

    feat : fix AssemblyStation

--- src/main/java/model/station/AssemblyStation.java
+++ src/main/java/model/station/AssemblyStation.java
@@ -1,23 +1,17 @@
 
 package model.station;
 
-import model.position.*;
-import model.chef.*;
-import model.interfaces.Preparable;package model.station;
-
-import model.position.*;
-import model.chef.*;
-import model.interfaces.Preparable;
 import model.chef.Chef;
 import model.enums.StationType;
+import model.item.Ingredient;
+import model.item.IngredientCombiner;
 import model.item.Item;
 import model.item.utensils.Plate;
-import model.enums.*;
+import model.interfaces.Preparable;
 import model.position.Position;
 
 public class AssemblyStation extends Station {
-
- public AssemblyStation(Position position) {
+    public AssemblyStation(Position position) {
         super(position, StationType.ASSEMBLY);
     }
 
@@ -25,9 +19,13 @@ public class AssemblyStation extends Station {
     public void interact(Chef chef) {
         if (PlatingHelper.handlePlateInHand(chef, this)) {
             return;
-            }
+        }
 
         if (PlatingHelper.handleUtensilInHand(chef, this)) {
+            return;
+        }
+
+        if (tryCombine(chef)) {
             return;
         }
 
@@ -38,14 +36,59 @@ public class AssemblyStation extends Station {
             chef.setInventory(null);
             return;
         }
+
         if (heldItem == null && hasItem()) {
-            chef.setInventory(itemOnStation);
+            chef.setInventory(getItemOnStation());
             removeItemFromStation();
         }
+    }
+
+    private boolean tryCombine(Chef chef) {
+        Item heldItem = chef.getInventory();
+        Item stationItem = getItemOnStation();
+
+        Ingredient heldIngredient = asIngredient(heldItem);
+        Ingredient stationIngredient = asIngredient(stationItem);
+
+        if (heldIngredient == null || stationIngredient == null) {
+            return false;
+        }
+
+        if (!IngredientCombiner.canCombine(heldIngredient, stationIngredient)) {
+            return false;
+        }
+
+        Ingredient combined = IngredientCombiner.combine(heldIngredient, stationIngredient);
+        if (combined == null) {
+            return false;
+        }
+
+        chef.setInventory(combined);
+        removeItemFromStation();
+        return true;
+    }
+
+    private Ingredient asIngredient(Item item) {
+        if (item instanceof Ingredient) {
+            return (Ingredient) item;
+        }
+        return null;
     }
 
     @Override
     public boolean canInteract(Chef chef) {
         return true;
+    }
+
+    @Override
+    public String getInteractionPrompt() {
+        Item stationItem = getItemOnStation();
+        if (stationItem instanceof Plate) {
+            return "Press C to take plate";
+        }
+        if (stationItem instanceof Preparable) {
+            return "Press C to pick up ingredient";
+        }
+        return "Press C to place item";
     }
 }
